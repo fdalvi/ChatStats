0899b2e9c62f57ef2579cb3603f4e30965d67794
Fahim Imaduddin Dalvi <dalvifahim@gmail.com>
2017-10-17 23:11:32 +0300

    Skeleton setup complete.
    Simple webapp so far to drag and drop/manually choose a file that
    is read by the app. Simple checking for text only format included!

---
+++ index.js
@@ -0,0 +1,74 @@
+function checkFileFormat(file) {
+	var reader = new FileReader();
+
+	// Closure to capture the file information.
+	reader.onload = function(e) {
+		text = e.target.result;
+		console.log("Read file...");
+	}
+
+	// Read in the image file as a data URL.
+	reader.readAsText(file);
+}
+
+function computeStatistics(file) {
+
+}
+
+function drawPlots(file) {
+
+}
+
+function analyzeChatBackup(file) {
+	$("#dropzone").addClass("hidden");
+
+	checkFileFormat(file)
+	computeStatistics(file)
+	drawPlots(file)
+}
+
+function handleFileSelect(evt) {
+	evt.stopPropagation();
+	evt.preventDefault();
+
+	$("#dropzone").css("background-color", "transparent");
+
+	var holder = evt.dataTransfer || evt.target;
+	var file = holder.files[0]; // FileList object.
+
+	if (!file.type.match('text.*')) {
+		$(".warning").removeClass("hidden");
+		return;
+	}
+
+	analyzeChatBackup(file);
+}
+
+function handleDragOver(evt) {
+	evt.stopPropagation();
+	evt.preventDefault();
+	evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+	$("#dropzone").css("background-color", "#F2FAF3")
+}
+
+function handleDragLeave(evt) {
+	$("#dropzone").css("background-color", "transparent");
+}
+
+window.onload = function() {
+	// Check for the various File API support.
+	if (window.File && window.FileReader && window.FileList && window.Blob) {
+		// Great success! All the File APIs are supported.
+	} else {
+		alert('The File APIs are not fully supported in this browser.');
+	}
+
+	// Setup the dnd listeners.
+	var dropZone = document.getElementById('dropzone');
+	dropZone.addEventListener('dragleave', handleDragLeave, false);
+	dropZone.addEventListener('dragover', handleDragOver, false);
+	dropZone.addEventListener('drop', handleFileSelect, false);
+
+	var manualSelect = document.getElementById('manual-select');
+	manualSelect.addEventListener('change', handleFileSelect, false);
+}
